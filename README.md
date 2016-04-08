5a7e03c5822dcc1302f8c4dc8fb7c01ab52e9012
rh <ryanjharvey88@gmail.com>
2016-04-08 16:24:02 -0700

    Finalized Page and Created README

---
+++ README.md
@@ -0,0 +1,33 @@
+# Pizza Monster
+
+#### A practice webpage for Epicodus, 4/8/2016
+
+### by Ryan Harvey
+
+## Description
+
+_This is a webpage for a fictional pizza restaurant designed to take a pizza order from a customer, calculate the order price and output the order information._
+
+## Setup
+
+_Clone this repository_
+
+_Open the index.html file in your preferred web browser_
+
+_This site can also be viewed at: ribalding.github.io/pizza_
+
+## Technologies Used
+
+_HTML_
+
+_CSS_
+
+_Javascript_
+
+_Jquery_
+
+_Bootstrap_
+
+## Legal
+
+_Copyright (c) 2016 Ryan Harvey_
--- js/my.js
+++ js/my.js
@@ -16,15 +16,19 @@ Pizza.prototype.calculatePrice = function(){
     this.price += 15;
   }
 
-  for(var i = 0; i < 2; i += 1){
+  for(var i = 0; i < 3; i += 1){
     if (this.toppings[i] === "pepperoni"){
       this.price += 2;
     } else if (this.toppings[i] === "peppers"){
       this.price += 1;
-    } else if(this.toppings[i] === "onions"){
+    } else if (this.toppings[i] === "onions"){
       this.price += 1;
     } else if (this.toppings[i] === "chicken"){
       this.price += 3;
+    } else if (this.toppings[i] === "bbq"){
+      this.price += 1;
+    } else if (this.toppings[i] === "sausage"){
+      this.price += 2;
     }
   }
   return this.price;
@@ -37,24 +41,32 @@ function Customer(passedName, passedAddress, passedPhone) {
   this['phone'] = passedPhone;
 }
 
-//UI Logic
+var validate = function(validateName, validatePhone, validateAddress) {
+  if (validateName === "" || validatePhone === "" || validateAddress[0] === "" || validateAddress[1] === "" || validateAddress[2] === "" || validateAddress[3] === ""){
+    alert("Woah, You Need To Fill In All Your Info Before You Can Become A Pizza Monster");
+    location.reload();
+  } else {
+    return true;
+  }
+}
 
+//UI Logic
+var inputPizzaSize2 = "";
+var inputToppings2 = [];
 $(document).ready(function() {
   $("form#pizzaForm").submit(function(event){
     event.preventDefault();
 
-    //Hide Input Form and Show Receipt
+    //Hide Input Form
     $('#formDiv').hide();
-    $('#receiptDiv').show();
 
-    //Creates new Pizza Object
+    //Creates new Pizza and Customer Objects
     var inputPizzaSize = $("#pizzaSize").val();
     var inputToppings = [];
     inputToppings.push($("#toppingOne").val());
     inputToppings.push($("#toppingTwo").val());
-    var newPizza = new Pizza(inputPizzaSize, inputToppings);
+    inputToppings.push($("#toppingThree").val());
 
-    //Create New Customer Object
     var inputName = $("#name").val();
     var inputPhone = $("#phone").val();
     var inputAddress = [];
@@ -62,22 +74,32 @@ $(document).ready(function() {
     inputAddress.push($("#city").val());
     inputAddress.push($("#state").val());
     inputAddress.push($("#zip").val());
-    newCustomer = new Customer(inputName, inputAddress, inputPhone);
 
-    //Creates Receipt
-    $(".outputName").text(newCustomer.customerName);
-    $(".outputSize").text(newPizza.pizzaSize);
-    if (newPizza.toppings[0] !== "none" && newPizza.toppings[1] !== "none"){
-      $(".outputToppings").text(" with " + newPizza.toppings[0] + " and " + newPizza.toppings[1]);
-    } else if (newPizza.toppings[0] !== "none" && newPizza.toppings[1] === "none" ){
-      $(".outputToppings").text(" with " + newPizza.toppings[0]);
+    //Validate Input
+    var validation = validate(inputName, inputPhone, inputAddress);
+    if (validation = true){
+      var newPizza = new Pizza(inputPizzaSize, inputToppings);
+      var newCustomer = new Customer(inputName, inputAddress, inputPhone);
+
+      //Creates Receipt
+      $(".outputName").text(newCustomer.customerName);
+      $(".outputSize").text(newPizza.pizzaSize);
+      if (newPizza.toppings[0] !== "none" && newPizza.toppings[1] !== "none" && newPizza.toppings[2] !== "none"){
+        $(".outputToppings").text(" with " + newPizza.toppings[0] + ", " + newPizza.toppings[1] + " and " + newPizza.toppings[2]);
+      } else if (newPizza.toppings[0] !== "none" && newPizza.toppings[1] === "none" ){
+        $(".outputToppings").text(" with " + newPizza.toppings[0]);
+      } else if (newPizza.toppings[0] !== "none" && newPizza.toppings[1] !== "none" && newPizza.toppings[2] === "none"){
+        $(".outputToppings").text(" with " + newPizza.toppings[0] + " and " + newPizza.toppings[1]);
+      }
+
+      $(".outputPrice").text("$" + newPizza.calculatePrice());
+      $(".outputStreet").text(newCustomer.address[0]);
+      $(".outputCity").text(newCustomer.address[1]);
+      $(".outputState").text(newCustomer.address[2]);
+      $(".outputZip").text(newCustomer.address[3]);
+
+      //Show Receipt
+      $('#receiptDiv').slideDown();
     }
-    $(".outputPrice").text("$" + newPizza.calculatePrice());
-    $(".outputStreet").text(newCustomer.address[0]);
-    $(".outputCity").text(newCustomer.address[1]);
-    $(".outputState").text(newCustomer.address[2]);
-    $(".outputZip").text(newCustomer.address[3]);
-
-
   });
 });
